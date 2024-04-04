600fae965acec346ee03efc0f0a98aba66ae504e
PieterJanDelie <pieterjan.delie@gmail.com>
2024-04-04 09:20:39 +0200

    Bugfix zie ook antwoord scherm van laatste vraag

--- Views/quiz.py
+++ Views/quiz.py
@@ -4,15 +4,17 @@ from flask import Blueprint, render_template, request, session
 
 quiz_view = Blueprint('quiz', __name__, template_folder='templates')
 
-def get_random_question():
+def get_random_question(asked_questions):
     with open('Data/QuizVragen.json', 'r') as f:
         vragen = json.load(f)['vragen']
-        return random.choice(vragen)
+        beschikbare_vragen = [vraag for vraag in vragen if vraag['vraag'] not in asked_questions]
+        return random.choice(beschikbare_vragen)
 
 @quiz_view.route("/")
 def quiz_home():
     session['questions_answered'] = 0
     session['correct_answers'] = 0
+    session['asked_questions'] = []
     return render_template("Quiz/quiz.html")
 
 @quiz_view.route("/StartQuiz", methods=['GET', 'POST'])
@@ -22,10 +24,12 @@ def start_quiz():
             eindscore = session.get('correct_answers', 0)
             session.pop('questions_answered', None)
             session.pop('correct_answers', None)
+            session.pop('asked_questions', None)
             return render_template("Quiz/quiz_end.html", eindscore=eindscore)
         else:
-            vraag_data = get_random_question()
+            vraag_data = get_random_question(session.get('asked_questions', []))
             vraag = vraag_data['vraag']
+            session['asked_questions'].append(vraag)
             mogelijke_antwoorden = vraag_data['mogelijke_antwoorden']
             session['current_quiz_question'] = vraag_data
             return render_template("Quiz/quiz_start.html", vraag=vraag, mogelijke_antwoorden=mogelijke_antwoorden)
@@ -36,12 +40,6 @@ def start_quiz():
         answer = request.form['answer']
         if answer == juist_antwoord:
             session['correct_answers'] = session.get('correct_answers', 0) + 1
-
-        if session['questions_answered'] >= 5:
-            eindscore = session.get('correct_answers', 0)
-            session.pop('questions_answered', None)
-            session.pop('correct_answers', None)
-            return render_template("Quiz/quiz_end.html", eindscore=eindscore)
 
         if answer == juist_antwoord:
             return render_template("Quiz/quiz_correct.html", antwoord=juist_antwoord, laatste_tekstje=vraag_data["laatste_tekstje"])
