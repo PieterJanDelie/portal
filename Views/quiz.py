b112b9a6367ff6bdfddb9ad663669e1d85688d31
PieterJanDelie <pieterjan.delie@gmail.com>
2024-04-04 08:42:18 +0200

    beginscherm quiz

--- Views/quiz.py
+++ Views/quiz.py
@@ -1,6 +1,6 @@
 import json
 import random
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect, url_for
 
 quiz_view = Blueprint('quiz', __name__, template_folder='templates')
 
@@ -8,16 +8,28 @@ def get_random_question():
     with open('Data/QuizVragen.json', 'r') as f:
         vragen = json.load(f)['vragen']
         return random.choice(vragen)
+    
+@quiz_view.route("/")
+def quiz_home():
+    session['questions_answered'] = 0  # Reset het aantal beantwoorde vragen
+    return render_template("Quiz/quiz.html")
 
-@quiz_view.route("", methods=['GET', 'POST'])
-def quiz():
+@quiz_view.route("/StartQuiz", methods=['GET', 'POST'])
+def start_quiz():
     if request.method == 'GET':
-        vraag_data = get_random_question()
-        vraag = vraag_data['vraag']
-        mogelijke_antwoorden = vraag_data['mogelijke_antwoorden']
-        session['current_quiz_question'] = vraag_data
-        return render_template("Quiz/quiz.html", vraag=vraag, mogelijke_antwoorden=mogelijke_antwoorden)
+        if session.get('questions_answered', 0) >= 5:
+            return redirect(url_for('quiz_home'))
+        else:
+            vraag_data = get_random_question()
+            vraag = vraag_data['vraag']
+            mogelijke_antwoorden = vraag_data['mogelijke_antwoorden']
+            session['current_quiz_question'] = vraag_data
+            return render_template("Quiz/quiz_start.html", vraag=vraag, mogelijke_antwoorden=mogelijke_antwoorden)
     elif request.method == 'POST':
+        session['questions_answered'] += 1
+        if session['questions_answered'] >= 5:
+            return redirect(url_for('quiz_home'))
+        
         answer = request.form['answer']
         vraag_data = session.get('current_quiz_question')
         juist_antwoord = vraag_data['juist_antwoord']
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from flask import Flask, request, render_template, session
-from config import Config
 from Views.home import home
 from Views.kalender import kalender
 from Views.stand import stand
 from Views.uitslagen import uitslagen
 from Views.team import team
-from Views.quiz import quiz
+from Views.quiz import start_quiz
+from Views.quiz import quiz_home
 from Views.feedback import feedback
 
 app = Flask(__name__)
 app.secret_key = 'BAD_SECRET_KEY'
-app.config.from_object(Config)
 
 app.add_url_rule("/", view_func=home)
 app.add_url_rule("/Kalender", view_func=kalender)
 app.add_url_rule("/Stand", view_func=stand)
 app.add_url_rule("/Uitslagen", view_func=uitslagen)
 app.add_url_rule("/Team", view_func=team)
-app.add_url_rule("/Quiz", view_func=quiz, methods=['GET', 'POST'])
+app.add_url_rule("/Quiz", view_func=quiz_home)
+app.add_url_rule("/Quiz/StartQuiz", view_func=start_quiz, methods=['GET', 'POST'])
 app.add_url_rule("/Feedback", view_func=feedback, methods=['GET', 'POST'])
 
 
