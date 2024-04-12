5180ddb0bc3fdfc25324f45e11d9cf755185fe6c
PieterJanDelie <pieterjan.delie@gmail.com>
2024-04-12 16:18:10 +0200

    Rebussen

---
+++ Views/rebus.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, render_template, session
+import os
+from datetime import datetime
+from services.gameFuncties import get_image_paths, get_random, write_to_csv, getRandomImage
+
+rebus_view = Blueprint('rebus', __name__, template_folder='templates')
+
+
+@rebus_view.route("/")
+def rebus_home():
+    session["woord"]=""
+    background_source=getRandomImage("spelers", InParentFolder=False)
+    return render_template("Rebus/rebus.html", background_source=background_source, imagesources=get_image_paths())
+
+
+@rebus_view.route("/Play")
+def rebus_game():
+    background_source=getRandomImage("spelers", InParentFolder=False)
+    rebus_source=getRandomImage("rebus/Rebussen", InParentFolder=False)
+    filename = os.path.basename(rebus_source)
+    filename_without_extension = os.path.splitext(filename)[0]
+    session["woord"] = filename_without_extension
+    print(filename_without_extension)
+    return render_template("Rebus/rebus_start.html", background_source="../"+background_source, rebus_source="../"+rebus_source, woord=filename_without_extension)
+    
+
+@rebus_view.route("/Einde/Geraden")
+def rebus_end_geraden():
+    background_source=getRandomImage("rebus/eindscherm/goed")
+    woord = session["woord"]
+    write_to_csv("Rebus/Eindscores.csv", [datetime.now(), woord, "Geraden"])
+    return render_template("Rebus/rebus_end_geraden.html", background_source="../" + background_source, woord=woord)
+
+@rebus_view.route("/Einde/NGeraden")
+def rebus_end_N_geraden():
+    background_source=getRandomImage("rebus/eindscherm/slecht")
+    woord = session["woord"]
+    write_to_csv("Rebus/Eindscores.csv", [datetime.now(), woord, "Niet geraden"])
+    return render_template("Rebus/rebus_end_N_geraden.html", background_source="../" + background_source, woord=woord)
