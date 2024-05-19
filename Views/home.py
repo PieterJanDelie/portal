fb244472b287295332ae1e6e6a3ca3b939becae8
PieterJanDelie <pieterjan.delie@gmail.com>
2024-05-19 16:38:46 +0200

    Image Load error Homescreen

--- Views/home.py
+++ Views/home.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from services.algemeneFuncties import get_image_paths, getRandomImage
 
 home_view = Blueprint('home', __name__, template_folder='templates')
 
 @home_view.route("")
 def home():
-    return render_template("Home/home.html", imagesources=get_image_paths(),  background_source=getRandomImage("Sfeer"))
+    print("load")
+
+    first_visit = 'visited' not in session
+    if first_visit:
+        session['visited'] = True
+
+    sources = get_image_paths() if first_visit else []
+    return render_template("Home/home.html", imagesources=sources, background_source=getRandomImage("Sfeer"))
